67f1f5ca3ad2d7f892caa10c87cf579ff071d4ba
jeong012 <ldj10235@gmail.com>
2022-06-21 17:41:49 +0900

    회원가입2 진행중
    joinForm2 회원가입 ajax 진행중

--- FDProject/src/main/java/com/fdproject/controller/UserController.java
+++ FDProject/src/main/java/com/fdproject/controller/UserController.java
@@ -3,9 +3,12 @@ package com.fdproject.controller;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -42,7 +45,7 @@ public class UserController extends UiUtils {
 
     /** 회원가입 - 질병 리스트 조회 사용*/
     @ResponseBody
-    @GetMapping(value = "/disease/getList.do")
+    @GetMapping("/disease/getList.do")
     public HashMap<String, Object> getUserDiseaseList(Model model) {
         List<DiseaseDTO> diseaseList = diseaseService.getUserDiseaseList();
         
@@ -53,7 +56,7 @@ public class UserController extends UiUtils {
 
     /** 회원가입 - 약 리스트 조회 사용*/
     @ResponseBody
-    @GetMapping(value = "/drug/getList.do")
+    @GetMapping("/drug/getList.do")
     public HashMap<String, Object> getUserDrugList(Model model) {
         List<DrugDTO> drugList = drugService.getUserDrugList();
         
@@ -61,5 +64,18 @@ public class UserController extends UiUtils {
         drugMap.put("drugList", drugList);
         return drugMap;
     }
-
+    
+    @ResponseBody
+    @PostMapping("/join.do")
+    public HashMap<String, Object> join(Model model, HttpServletRequest request) {
+    	System.out.println(request.getParameter("drugMap"));
+    	System.out.println(request.getParameter("diseaseMap"));
+    	
+    	HashMap<String, Object> resultMap = new HashMap<String, Object>();
+    	resultMap.put("result", "success");
+    	
+    	return resultMap;
+    }
+    
+    
 }
