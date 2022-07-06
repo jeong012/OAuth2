e461a24744aaa6445370a8a70a547047d080e3f8
jeong012 <ldj10235@gmail.com>
2022-07-06 15:29:08 +0900

    Update deploy.sh

--- study01-springboot2-webservice/scripts/deploy.sh
+++ study01-springboot2-webservice/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step2/zip
+REPOSITORY=/home/ec2-user/app/step2/
 PROJECT_NAME=study01-springboot2-webservice
 
 echo "> Build 파일 복사"
@@ -36,4 +36,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  $JAR_NAME > $REPOSITORY/zip/nohup.out 2>&1 &
